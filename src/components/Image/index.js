3ee263299bcaff006defc56d8152b2916ad7b0a7
BrittGall <crlnfllr@gmail.com>
2019-12-24 19:12:09 -0600

    added isCLicked to props to test

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Image({ img, name, id, handleClick }) {
+function Image({ img, name, id, handleClick, isClicked }) {
   return (
     <div className="column is-one-quarter">
       <div className="box">
         <figure>
           <img src={img} alt={name} id={id} onClick={handleClick} />
         </figure>
+        <p>{isClicked.toString()}</p>
       </div>
     </div>
   );
