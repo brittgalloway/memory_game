eb8c891dbc647f9a37510485c4f2d499c18f66f4
BrittGall <crlnfllr@gmail.com>
2019-12-24 22:35:17 -0600

    15 imgs, css edits

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Image({ img, name, id, handleClick, isClicked }) {
   return (
-    <div className="column is-one-quarter">
+    <div className="column is-one-fifth    ">
       <div className="box">
         <figure>
           <img src={img} alt={name} id={id} onClick={handleClick} />
